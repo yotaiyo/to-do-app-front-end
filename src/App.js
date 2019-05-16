fce5591b83cc233727222ab7f5a6626959abc668
yotaiyo <yota071218@gmail.com>
2019-05-16 15:55:58 +0900

    create Title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import './App.css'
+
+const Wrapper = styled.div``
+
+const Title = styled.div`
+  font-size: 30px;
+  box-shadow: 0px 5px 10px -3px rgba(0,0,0,0.5);
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper className="App">
+      <Title>yotaiyo`s To-Do App</Title>
+    </Wrapper>
   );
 }
 
